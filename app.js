de20a547c8e7eb6748fa72e040b35031a7eef261
nuwandarshika <nu1darshika@gmail.com>
2024-03-30 21:50:01 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('successfully connected to server');
+})
+
+app.listen('3000', () => {
+    console.log('App is lisstning to port 3000')
+})
