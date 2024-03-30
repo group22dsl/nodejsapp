5127ddbc4b5333abe8494dc0303287adafe5068c
nuwandarshika <nu1darshika@gmail.com>
2024-03-30 23:26:00 +0100

    Added jest config

--- test/app.test.js
+++ test/app.test.js
@@ -5,5 +5,6 @@ describe('Sample test' , () => {
     it('Test / endpoint', async () => {
         const response = await request(app).get('/');
         expect(response.text).toBe('successfully connected to server');
+    
     })
 })
