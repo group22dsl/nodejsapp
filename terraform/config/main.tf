f11e0e230c70cae29732a82c105509915af9885d
nuwandarshika <nu1darshika@gmail.com>
2024-04-01 04:48:16 +0200

    test

--- terraform/config/main.tf
+++ terraform/config/main.tf
@@ -6,37 +6,64 @@ provider "google" {
 }
 
 # Define the Google kubernates engine cluster
-resource "google_container_cluster" "google-cloud-cluster-2" {
-    name = "google-cloud-cluster-2"
-    location = "europe-west1"
-    initial_node_count = 1
+resource "google_container_cluster" "my_cluster" {
+  name               = "my-gke-cluster"
+  location           = "europe-west1"
+  initial_node_count = 3
 
-    addons_config {
-        http_load_balancing {
-            disabled = false
-        }
-        horizontal_pod_autoscaling {
-            disabled = false
-        }
+  # Optional: Add-ons configuration
+  addons_config {
+    http_load_balancing {
+      disabled = false
     }
-
-    maintenance_policy {
-        daily_maintenance_window {
-            start_time = "03:00"
-        }
+    horizontal_pod_autoscaling {
+      disabled = false
     }
+  }
+# resource "google_container_cluster" "google-cloud-cluster-2" {
+#     name = "google-cloud-cluster-2"
+#     location = "europe-west1"
+#     initial_node_count = 1
 
-    remove_default_node_pool = true
+#     addons_config {
+#         http_load_balancing {
+#             disabled = false
+#         }
+#         horizontal_pod_autoscaling {
+#             disabled = false
+#         }
+#     }
+
+#     maintenance_policy {
+#         daily_maintenance_window {
+#             start_time = "03:00"
+#         }
+#     }
+
+#     remove_default_node_pool = true
+# }
+
+# Fetch information about the GKE cluster
+data "google_container_cluster" "my_cluster_data" {
+  name     = google_container_cluster.my_cluster.name
+  location = google_container_cluster.my_cluster.location
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Path to your kubeconfig file
+  version     = "~> 2.0"          # Version constraint for the provider
+  host        = "https://${data.google_container_cluster.my_cluster_data.endpoint}"
 }
 
 # Define the Kubernetes provider
-provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.google-cloud-cluster-2.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.google-cloud-cluster-2.master_auth[0].cluster_ca_certificate,
-  )
-}
+# provider "kubernetes" {
+#   host  = "https://${data.google_container_cluster.google-cloud-cluster-2.endpoint}"
+#   token = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(
+#     data.google_container_cluster.google-cloud-cluster-2.master_auth[0].cluster_ca_certificate,
+#   )
+# }
+
 
 # Define the Kubenetes deployment for the node.js application
 
